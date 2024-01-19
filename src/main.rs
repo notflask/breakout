f38d0bba2f84776993b95b19b8f3f58d6a93a393
Danilo Skyba <dan.sskyba@gmail.com>
2024-01-19 11:54:23 +0100

    feat: add basic macroquad code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("breakout")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        next_frame().await
+    }
 }
